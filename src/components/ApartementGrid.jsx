e04f862c7b8ec65e05be2bc00fe64d6a9002b3b8
itsArgos <drop_m37@hotmail.com>
2023-12-21 18:28:51 +0100

    Ajout du dossier components (Navbar/Banner/ApartementGrid)

---
+++ src/components/ApartementGrid.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ApartementGrid.scss'
+
+function ApartementGrid() {
+  return (
+    <div className='grid'>ApartementGrid</div>
+  )
+}
+
+export default ApartementGrid
---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.scss'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <h1 className='banner_title'>Chez vous, partout et ailleurs</h1>
+    </div>
+  )
+}
+
+export default Banner
